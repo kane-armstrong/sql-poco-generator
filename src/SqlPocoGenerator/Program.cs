ab7d3c1f240257a525b3bdb6367c51ef5dddc32e
Kane Armstrong <kane.armstrong@outlook.com>
2020-10-25 17:34:21 +1300

    Refactor startup

--- src/SqlPocoGenerator/Program.cs
+++ src/SqlPocoGenerator/Program.cs
@@ -1,53 +1,20 @@
-﻿using System;
-using System.IO;
-using System.Threading;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SqlPocoGenerator.Services;
 using System.Threading.Tasks;
-using SqlPocoGenerator.Poco;
-using SqlPocoGenerator.Schema;
 
 namespace SqlPocoGenerator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Task Main()
         {
-            RunProgram().GetAwaiter().GetResult();
-        }
-
-        static async Task RunProgram()
-        {
-            Console.WriteLine("--------Class generator--------");
-            Console.WriteLine("I can generate C# POCOs from tables in a MSSQL database");
-            Console.WriteLine("Example connection string: Persist Security Info=False;User ID=MY_SQL_USER;Password=MY_SQL_PASSWORD;Server=localhost,1433; Initial Catalog=MY_CATALOG_NAME");
-            Console.Write("Enter connection string: ");
-            var cs = Console.ReadLine();
-            Console.Write("Enter namespace: ");
-            var namespaceName = Console.ReadLine();
-            Console.Write("Enter output path: ");
-            var outputPath = Console.ReadLine();
-
-            var explorer = new SqlServerSchemaExplorer(cs);
-            var tables = await explorer.GetTablesAsync(CancellationToken.None);
-            var renderer = new ClassRenderer();
-            //            var renderer = new ClassRendererV2();
-            foreach (var table in tables)
-            {
-                Console.WriteLine("Class:");
-                var content = renderer.Render(new ClassTemplate
+            return new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
                 {
-                    Namespace = namespaceName,
-                    Properties = table.Columns,
-                    TypeName = table.Name.LegalCsharpName
-                });
-                Console.WriteLine(content);
-                var fileName = Path.Join(outputPath, $"{table.Name.LegalCsharpName}.cs");
-                Console.WriteLine($"Saving it to disk, see {fileName}");
-                File.WriteAllLines(fileName, content.Split(Environment.NewLine));
-            }
-
-            Console.WriteLine("Done.");
-
-            Console.ReadKey();
+                    services.AddHostedService<SqlPocoGeneratorService>();
+                })
+                .RunConsoleAsync();
         }
     }
 }
---
+++ src/SqlPocoGenerator/Services/SqlPocoGeneratorService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using SqlPocoGenerator.Poco;
+using SqlPocoGenerator.Schema;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SqlPocoGenerator.Services
+{
+    public class SqlPocoGeneratorService : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("--------Class generator--------");
+            Console.WriteLine("I can generate C# POCOs from tables in a MSSQL database");
+            Console.WriteLine("Example connection string: Persist Security Info=False;User ID=MY_SQL_USER;Password=MY_SQL_PASSWORD;Server=localhost,1433; Initial Catalog=MY_CATALOG_NAME");
+            Console.Write("Enter connection string: ");
+            var cs = Console.ReadLine();
+            Console.Write("Enter namespace: ");
+            var namespaceName = Console.ReadLine();
+            Console.Write("Enter output path: ");
+            var outputPath = Console.ReadLine();
+
+            var explorer = new SqlServerSchemaExplorer(cs);
+            var tables = await explorer.GetTablesAsync(CancellationToken.None);
+            var renderer = new ClassRenderer();
+
+            foreach (var table in tables)
+            {
+                Console.WriteLine("Class:");
+                var content = renderer.Render(new ClassTemplate
+                {
+                    Namespace = namespaceName,
+                    Properties = table.Columns,
+                    TypeName = table.Name.LegalCsharpName
+                });
+                Console.WriteLine(content);
+                var fileName = Path.Join(outputPath, $"{table.Name.LegalCsharpName}.cs");
+                Console.WriteLine($"Saving it to disk, see {fileName}");
+                File.WriteAllLines(fileName, content.Split(Environment.NewLine));
+            }
+
+            Console.WriteLine("Done.");
+
+            Console.ReadKey();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
